7c3f4ca18bb431a18fc7caf58fdb6ea0d881293d
AlexanderGuideon <49411610+AlexanderGuideon@users.noreply.github.com>
2019-05-17 13:23:39 +0200

    Update ConvierteCadenasV2.java

--- ProyectoCalculadora/app/src/main/java/com/example/proyectocalculadora/ConvierteCadenasV2.java
+++ ProyectoCalculadora/app/src/main/java/com/example/proyectocalculadora/ConvierteCadenasV2.java
@@ -1,66 +1,109 @@
-class ConvierteCadenas{
+public class ConvierteCadenas{
 
   static boolean esNum = false;
   static boolean esSimbolo = false;
   static boolean esFun = false;
   
   public static void main(String[]args){
-    String cadena = "ochomil doscientos doce";
+    String cadena = "ochocientoscincomil doscientos doce";
     
     String palabras [] = cadena.split(" ");
     String resul = "";
-   
+    String palabra = "";
+    
     for(int i = 0; i<palabras.length;i++){
-    	String palabra = palabras[i]; 
-       	resul = conversion(palabra,resul);    	
- 
+        resul += conversion(palabras[i]);
     }
     
-  }
-  
-  public static String conversion(String cadena, String resul){
-  
     if(cadena.endsWith("diez")||cadena.endsWith("enta")||cadena.endsWith("einte"))
   		resul += "0";
   	else if(cadena.endsWith("mil"))
   		resul += "000";
   	else if(cadena.endsWith("millon"))
-  		resul += "000000";
-    
-    else{
+  	    resul += "000000";
+  	    
+    System.out.println(resul);
+  }
   
-  	if(cadena.contains("un")||cadena.startsWith("dieci")||cadena.startsWith("ciento"))
-  		resul += "1"; 
-  	if(cadena.contains("dos")||cadena.startsWith("veinti")||cadena.startsWith("dosciento"))
-  		resul += "2";
-  	if(cadena.contains("tres")||cadena.startsWith("treinta")||cadena.startsWith("tresciento"))
-  		resul += "3";
-  	if(cadena.contains("cuatro")||cadena.startsWith("cuarenta")||cadena.startsWith("cuatrociento"))
-  		resul += "4";
-  	if(cadena.contains("cinco")||cadena.startsWith("cincuenta")||cadena.startsWith("quiniento"))
-  		resul += "5";
-  	if(cadena.contains("seis")||cadena.startsWith("sesenta")||cadena.startsWith("seiciento"))
-  		resul += "6";
-  	if(cadena.contains("siete")||cadena.startsWith("setenta")||cadena.startsWith("seteciento"))
-  		resul += "7";
-  	if(cadena.contains("ocho")||cadena.startsWith("ochenta")||cadena.startsWith("ochocientos"))
-  		resul += "8";
-  	if(cadena.contains("nueve")||cadena.startsWith("noventa")||cadena.startsWith("novecientos"))
-  		resul += "9";
-  	if(cadena.contains("once"))
-  		resul += "11";
-  	if(cadena.contains("doce"))
-  		resul += "12";
-  	if(cadena.contains("trece"))
-  		resul += "13";
-  	if(cadena.contains("catorce"))
-  		resul += "14";
-  	if(cadena.contains("quince"))
-  		resul += "15";
-  	
-  	}
-  	
-  		
+  public static String conversion(String cadena){
+  
+        String resul = "";
+        
+        boolean esmil
+        boolean esciento = false;
+        boolean esdecena = false;
+        
+            
+            
+            if(cadena.startsWith("dieci")){
+                 resul += "1";
+                 esdecena = true;
+            }
+            if(cadena.startsWith("veinti")){
+                 resul += "2";
+                 esdecena = true;
+            }
+            if(cadena.startsWith("treinta")){
+                 resul += "3";
+                 esdecena = true;
+            }
+            if(cadena.startsWith("cuarenta")){
+                 resul += "4";
+                 esdecena = true;
+            }
+            if(cadena.startsWith("cincuenta")){
+                 resul += "5";
+                 esdecena = true;
+            }
+            if(cadena.startsWith("sesenta")){
+                 resul += "6";
+                 esdecena = true;
+            }
+            if(cadena.startsWith("setenta")){
+                 resul += "7";
+                 esdecena = true;
+            }
+            if(cadena.startsWith("ochenta")){
+                 resul += "8";
+                 esdecena = true;
+            }
+            if(cadena.startsWith("noventa")){
+                 resul += "9";
+                 esdecena = true;
+            }
+            
+            
+            
+  	        if(cadena.contains("un"))
+  		          resul += "1"; 
+  	        if(cadena.contains("dos"))
+  		          resul += "2";
+  	        if(cadena.contains("tres"))
+  		          resul += "3";
+  	        if(cadena.contains("cuatro"))
+  		          resul += "4";
+  	        if(cadena.contains("cinco"))
+  		          resul += "5";
+  	        if(cadena.contains("seis"))
+  		          resul += "6";
+  	        if(cadena.contains("siete"))
+  		          resul += "7";
+  	        if(cadena.contains("ocho"))
+  		          resul += "8";
+  	        if(cadena.contains("nueve"))
+  		          resul += "9";
+  	        if(cadena.contains("once"))
+  		          resul += "11";
+  	        if(cadena.contains("doce"))
+  		        resul += "12";
+  	        if(cadena.contains("trece"))
+  		        resul += "13";
+  	        if(cadena.contains("catorce"))
+  		        resul += "14";
+  	        if(cadena.contains("quince"))
+  		        resul += "15";
+  	   
       return resul;
   }
+  
 }
